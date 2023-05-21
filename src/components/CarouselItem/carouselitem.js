3afbdbae6e556cb82e23531714d936b4afe33cb9
anjaliruth <ruthanjali0211@gmail.com>
2023-05-21 16:34:17 +0800

    changed fonts

--- src/components/CarouselItem/carouselitem.js
+++ src/components/CarouselItem/carouselitem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-export const Carouselitem = ({ title, description, image, githubUrl, pageUrl }) => {
+export const Carouselitem = ({
+  title,
+  description,
+  image,
+  githubUrl,
+  pageUrl,
+}) => {
   function goToGithub() {
     window.open(githubUrl, "_blank");
   }
@@ -8,16 +14,15 @@ export const Carouselitem = ({ title, description, image, githubUrl, pageUrl }) 
     window.open(pageUrl, "_blank");
   }
 
-
   return (
     <div className="carousel-item">
       <img className="carousel-item-image" src={image} alt="project image" />
       <div className="carousel-content">
-        <h2 className="carousel-item-titile">{title}</h2>
-        <div className="carousel-item-text">{description}</div>
-        <div className="carousel-buttons" >
-        <button onClick={goToPage}>URL</button>
-        <button onClick={goToGithub}>Github</button>
+        <h2 className="carousel-item-title">{title}</h2>
+        <p className="carousel-item-text">{description}</p>
+        <div className="carousel-buttons2">
+          <button onClick={goToPage}>URL</button>
+          <button onClick={goToGithub}>Github</button>
         </div>
       </div>
     </div>
