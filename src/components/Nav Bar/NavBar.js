c584ff95e77bdf3ce0b111cff1bb1ae64784867d
anjaliruth <ruthanjali0211@gmail.com>
2023-07-18 09:44:03 +0800

    added name to navbar

--- src/components/Nav Bar/NavBar.js
+++ src/components/Nav Bar/NavBar.js
@@ -3,25 +3,27 @@ import React from "react";
 function NavBar() {
   return (
     <div className="NavBar">
-    <nav>
       <ul className="sectionList">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
+        <li className="nameTab">Anjali Ruth Muthusamy</li>
       </ul>
+      <nav>
+        <ul className="sectionList">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
 }
-
 
 export default NavBar;
